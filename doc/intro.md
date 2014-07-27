b3314e95a4f25fa7703da16c1c4c32aa6dd140f2
Josh Lehman <jalehman37@gmail.com>
2014-07-27 15:03:04 -0700

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ring-transit
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject ring-transit "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.cognitect/transit-clj "0.8.229"]
+                 [prismatic/plumbing "0.3.3"]
+                 [ring/ring-core "1.3.0"]])
