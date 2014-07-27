3487b8183dc2b9081ed4dbab362f0a1139a82748
Josh Lehman <jalehman37@gmail.com>
2014-07-27 15:12:55 -0700

    match ns of ring-json

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject ring-transit "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Ring middleware for handling transit format"
+  :url "https://github.com/jalehman/ring-transit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
