24d9c2d6cf64f89388ef5d0975ebce1eb83757e4
Josh Lehman <jalehman37@gmail.com>
2014-07-27 16:45:12 -0700

    deploy ready

--- project.clj
+++ project.clj
@@ -1,8 +1,17 @@
 (defproject ring-transit "0.1.0"
   :description "Ring middleware for handling transit format"
   :url "https://github.com/jalehman/ring-transit"
+
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :scm {:name "git"
+        :url "https://github.com/jalehman/ring-transit"}
+
+  :signing {:gpg-key "jalehman37@gmail.com"}
+
+  :deploy-repositories [["clojars" {:creds :gpg}]]
+
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.cognitect/transit-clj "0.8.229"]
                  [prismatic/plumbing "0.3.3"]
