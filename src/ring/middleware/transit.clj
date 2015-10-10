d19b523f72aad35ff6a406282e1034cc9f3481db
Josh Lehman <jalehman37@gmail.com>
2015-10-10 13:50:09 -0700

    extend idempotence to wrap-transit-params

--- src/ring/middleware/transit.clj
+++ src/ring/middleware/transit.clj
@@ -4,11 +4,14 @@
             [plumbing.core :refer [keywordize-map]]
             [cognitect.transit :as transit]))
 
-(defn- write [x t opts]
+;; ================================================================================
+;; Helpers
+
+(defn- write [x t charset opts]
   (let [baos (ByteArrayOutputStream.)
         w    (transit/writer baos t opts)
         _    (transit/write w x)
-        ret  (.toString baos)]
+        ret  (.toString baos charset)]
     (.reset baos)
     ret))
 
@@ -34,6 +37,30 @@
    :headers {"Content-Type" "text/plain"}
    :body    "Malformed Transit in request body."})
 
+(defn- transit-response-options
+  [options]
+  {:encoding (get options :encoding :json)
+   :charset  (get options :charset "utf-8")
+   :opts     (:opts options)})
+
+(defn- transit-handler
+  [handler f sentinel-key {:keys [malformed-response malformed-response-fn]
+                           :or {malformed-response default-malformed-response}
+                           :as options}]
+  (fn [request]
+    (if (get request sentinel-key)
+      (handler request)
+      (if-let [[valid? transit-or-ex] (read-transit request options)]
+        (if valid?
+          (handler (assoc (f request transit-or-ex) sentinel-key true))
+          (if malformed-response-fn
+            (malformed-response-fn transit-or-ex request handler)
+            malformed-response))
+        (handler request)))))
+
+;; ================================================================================
+;; API
+
 (defn wrap-transit-body
   "Middleware that parses the body of Transit request maps, and replaces the :body
   key with the parsed data structure. Requests without a Transit content type are
@@ -47,25 +74,10 @@
   :malformed-response-fn   - a custom error handler that gets called when the body is malformed
                              transit. Will be called with three parameters: [exception request handler]"
   {:arglists '([handler] [handler options])}
-  [handler & [{:keys [malformed-response malformed-response-fn]
-               :or {malformed-response default-malformed-response}
-               :as options}]]
-  (fn [request]
-    (if (::wrap-transit-body request)
-      (handler request)
-      (if-let [[valid? transit-or-ex] (read-transit request options)]
-        (if valid?
-          (handler (assoc request :body transit-or-ex ::wrap-transit-body true))
-          (if malformed-response-fn
-            (malformed-response-fn transit-or-ex request handler)
-            malformed-response))
-        (handler request)))))
-
-(defn- assoc-transit-params [request transit]
-  (let [request (assoc request :transit-params transit)]
-    (if (map? transit)
-      (update-in request [:params] merge transit)
-      request)))
+  [handler & [{:as options}]]
+  (letfn [(-assoc-body [req res]
+            (assoc req :body res))]
+    (transit-handler handler -assoc-body ::wrap-transit-body options)))
 
 (defn wrap-transit-params
   "Middleware that parses the body of Transit requests into a map of parameters,
@@ -73,21 +85,31 @@
 
   Accepts the following options:
 
-  :malformed-response - a response map to return when the JSON is malformed
-  :opts               - a map of options to be passed to the transit reader
+  :opts                  - a map of options to be passed to the transit reader
+  :malformed-response    - a response map to return when the Transit is malformed
+  :malformed-response-fn - a custom error handler that gets called when the body is malformed
+                             transit. Will be called with three parameters: [exception request handler]
 
   Use the standard Ring middleware, ring.middleware.keyword-params, to
   convert the parameters into keywords."
   {:arglists '([handler] [handler options])}
-  [handler & [{:keys [malformed-response]
-               :or {malformed-response default-malformed-response}
-               :as options}]]
-  (fn [request]
-    (if-let [[valid? transit] (read-transit request options)]
-      (if valid?
-        (handler (assoc-transit-params request transit))
-        malformed-response)
-      (handler request))))
+  [handler & [{:as options}]]
+  (letfn [(-assoc-transit-params [req res]
+            (let [request (assoc req :transit-params res)]
+              (if (map? res)
+                (update-in request [:params] merge res)
+                request)))]
+    (transit-handler handler -assoc-transit-params ::wrap-transit-params options)))
+
+(defn transit-response
+  "Create a transit response based on options."
+  [response request {:keys [opts encoding charset]}]
+  (if (coll? (:body response))
+    (let [transit-response (update-in response [:body] write encoding charset opts)]
+      (if (contains? (:headers response) "Content-Type")
+        transit-response
+        (content-type transit-response (format "application/transit+%s; charset=%s" (name encoding) charset))))
+    response))
 
 (defn wrap-transit-response
   "Middleware that converts responses with a map or a vector for a body into a
@@ -99,14 +121,9 @@
   :opts     - a map of options to be passed to the transit writer"
   {:arglists '([handler] [handler options])}
   [handler & [{:as options}]]
-  (let [{:keys [encoding opts] :or {encoding :json}} options]
+  (let [{:keys [encoding] :as opts} (transit-response-options options)]
     (assert (#{:json :json-verbose :msgpack} encoding)
             "The encoding must be one of #{:json :json-verbose :msgpack}.")
     (fn [request]
-      (let [response (handler request)]
-        (if (coll? (:body response))
-          (let [transit-response (update-in response [:body] write encoding opts)]
-            (if (contains? (:headers response) "Content-Type")
-              transit-response
-              (content-type transit-response (format "application/transit+%s; charset=utf-8" (name encoding)))))
-          response)))))
+      (-> (handler request)
+          (transit-response request opts)))))
